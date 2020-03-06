bfdbe2849a18e0418207e0becc7071ddf9b20bee
Vladimir Moroz <vovanmoroz1509@gmail.com>
2020-03-06 11:57:05 +0200

    Base finished version

--- src/displayTemp.c
+++ src/displayTemp.c
@@ -12,6 +12,7 @@
 #include <arpa/inet.h>
 #include <stdlib.h>
 
+//LCD PINS
 #define rs_pin 30
 #define e_pin 21
 #define p_1 22
@@ -26,48 +27,75 @@ int main()
 {
 	
     char ip_addr[12];
+    char temp[3];
     int lcd = 0;
 	int fd = 0;
+	int data = 0;
+	int sda = 0;
 	struct ifreq ifr;
 	
+	
+	
+	//GET THE IP ADDRESS
 	fd = socket(AF_INET, SOCK_DGRAM, 0);
 	ifr.ifr_addr.sa_family = AF_INET;
-
     memcpy(ifr.ifr_name, "wlan0", IFNAMSIZ-1);
     ioctl(fd, SIOCGIFADDR, &ifr);
     close(fd);
     strcpy(ip_addr, inet_ntoa(((struct sockaddr_in*)&ifr.ifr_addr)->sin_addr));
 
-	int data = 0;
-	int sda = 0;
-	char temp[3];
-	
-	
-	
+	//INITIALIZE wiringPi
 	wiringPiSetup();
-	sda = wiringPiI2CSetup(0x4a); //GET DATA
 	
-
+	//SETUP LED PIN
+	pinMode(0,OUTPUT);
+	digitalWrite(0,0);
+	
+	//I2C READ ADDRESS
+	sda = wiringPiI2CSetup(0x4a);
+	
+	//INITIALIZE AND CLEAR LCD
 	lcd = lcdInit(2, 16, 4, rs_pin, e_pin, p_1, p_2, p_3, p_4, 0,0,0,0);
 	lcdClear(lcd);
 	lcdPosition(lcd, 0, 0);
+	
+	//PRINT THE DEVICE IP ADDRESS
 	lcdPrintf(lcd, "IP: %s", ip_addr);
 	
 	while(1)
 	{
+		//CLEAR PART OF THE LOWER HALF OF THE SCREEN
 		lcdPosition(lcd, 8, 1);
 		lcdPuts(lcd, "    ");
+		
+		//TURN OFF LED
+		digitalWrite(0,0);
+		
+		//GET DATA FROM I2C
 		data = wiringPiI2CRead(sda);
+		
+		//ADJUST TO GET NEGATIVE TEMPERATURE VALUE
 		if((data&0x80) != 0)
 		{
 				data = data & 0x7F;
 				data = data - 128;
 		}
-		printf("%d", data);
+		
+		//CONVERT DATA TO STRING
 		sprintf(temp, "%d", data);
+		
+		//PRINT TEMPERATURE
 		lcdPosition(lcd, 0, 1);
 		lcdPrintf(lcd,"Temp.: %s", temp);
-		delay(1000);		
+		
+		//TURN ON LED
+		if(data <= 0)
+		{
+			digitalWrite(0,1);
+		}
+		//else {digitalWrite(0,0);}
+		//DELAY
+		delay(700);		
 	}
 	
 	return 0;
